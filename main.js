ab411ee7563f65c355a606c72c17b977efd53810
lucasvergara1 <lucas.vergara@live.com>
2023-12-09 22:22:28 -0300

    :memo: Fixing stuff in general

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-const $root = document.querySelector("#root")
+import "./src/styles/settings/colors.css";
+import "./src/styles/generic/reset.css"
+import "./src/styles/elements/base.css";
 
-$root.textContent = "Lucas Vergara DEV"
+import CardGame from "./src/components/CardGame"; 
+
+
+const $root = document.querySelector("#root");
+const $htmlCardGame = CardGame();
+
+console.log($htmlCardGame);
+
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
 
