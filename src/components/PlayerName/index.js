f50ec4ecee05a6597557a8a2113241d6ebe0c810
lucasvergara1 <lucas.vergara@live.com>
2023-12-11 19:45:47 -0300

    :lipstick: Adjustments of CSS for Player name

--- src/components/PlayerName/index.js
+++ src/components/PlayerName/index.js
@@ -1,8 +1,9 @@
+import "./style.css"
+
 function PlayerName(content) {
     return `
-        <p>${content}</p>
+        <p class="player-name">${content}</p>
     `;
-
 }
 
 export default PlayerName;
