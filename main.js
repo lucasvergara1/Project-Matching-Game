5a42405229130975913daa260ebc0c807f66112d
lucasvergara1 <lucas.vergara@live.com>
2023-12-14 22:14:06 -0300

    Create new component - Cardfrontback

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css"
 import "./src/styles/elements/base.css";
-import BoardGame from "./src/objects/BoardGame";
-import PlayerName from "./src/components/PlayerName";
 import ScoreBoard from "./src/objects/ScoreBoard";
+import BoardGame from "./src/objects/BoardGame";
+
 
 const $root = document.querySelector("#root");
 
@@ -12,7 +12,7 @@ $root.insertAdjacentHTML(
     "beforeend", 
         `
         ${ScoreBoard()}
-        ${BoardGame(6)}
+        ${BoardGame(2)}
         `
 );
 
